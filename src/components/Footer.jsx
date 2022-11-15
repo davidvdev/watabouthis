58c6cff90e4c77df31ccca15a007f71c2c44b8c0
David Vogel <david@davidvdev.com>
2022-11-15 17:40:50 -0600

    styling adjustments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@
 const Footer = () => {
     return(
         <footer>
-            <a href="https://watabou.github.io/index.html">More Watabou</a>
+            <a className="bookEnd" href="https://watabou.github.io/index.html">More Watabou</a>
             <p>Have a theme you'd like to contribute? Submit a pull request on the <a href="https://github.com/davidvdev/watabouthis">Github repo!</a></p>
-            <a href="https://www.davidvdev.com">More davidvdev</a>
+            <a className="bookEnd" href="https://www.davidvdev.com">More davidvdev</a>
         </footer>
     )
 }
