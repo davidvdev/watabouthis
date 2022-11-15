9728e6319f9b006f2509de400b1bc737798ce2e2
David Vogel <david@davidvdev.com>
2022-11-15 16:34:57 -0600

    simplified header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom"
 const Header = ({title, link}) => {
     return(
         <header className="alt-header">
-            <Link to="/">Home</Link>
             <a href={link} rel="noreferer" target="_blank">
                 <h1>{title}</h1>
             </a>
+            <Link to="/">Back to Generator List</Link>
         </header>
     )
 }
