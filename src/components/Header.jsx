fee5e5340e285bd442bfb6dd044ee36ee656eff4
David Vogel <david@davidvdev.com>
2022-11-15 16:28:23 -0600

    styling and final touches for gen links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom"
-const Header = ({title}) => {
+const Header = ({title, link}) => {
     return(
         <header className="alt-header">
             <Link to="/">Home</Link>
-            <a>
-            <h1>{title}</h1>
+            <a href={link} rel="noreferer" target="_blank">
+                <h1>{title}</h1>
             </a>
         </header>
     )
