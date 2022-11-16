9ca986ea9c4358bf6c6ad1d9724bd9a991b38a3c
David Vogel <david@davidvdev.com>
2022-11-15 18:26:22 -0600

    changed to issues instead of PRs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return(
         <footer>
             <a className="bookEnd" href="https://watabou.github.io/index.html">More Watabou</a>
-            <p>Have a theme you'd like to contribute? Submit a pull request on the <a href="https://github.com/davidvdev/watabouthis">Github repo!</a></p>
+            <p>Have a theme you'd like to contribute? Submit an issue on the <a href="https://github.com/davidvdev/watabouthis">Github repo!</a></p>
             <a className="bookEnd" href="https://www.davidvdev.com">More davidvdev</a>
         </footer>
     )
